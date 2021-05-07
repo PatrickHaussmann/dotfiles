e5abd6e90fcebf6645a5a71ffc52ef343d653e6b
Patrick Haußmann <git@patrickhaussmann.de>
2021-05-07 17:25:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
-# Install stow
+# this install file is for github codespaces
+
+
+touch 1.txt
+
+## Install stow
 apt install -y stow
 
-# From the dotfiles dir
+touch 2.txt
+
+## From the dotfiles dir
 for x in */; do stow $x; done
+
+touch 3.txt
+
