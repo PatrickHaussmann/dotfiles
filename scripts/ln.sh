33e99c63cb050029a44a236149a0bc8800e8c7bd
patrickhaussmann <git@patrickhaussmann.de>
2021-05-25 14:06:59 +0200

    Fix symlink script
    link all files starting with a dot.
    for folders: create folder and link files inside.
    check if file exists before linking

--- scripts/ln.sh
+++ scripts/ln.sh
@@ -3,22 +3,33 @@
 # Up from scripts dir
 cd ..
 
-function link {
-  # check if it is file or directory
-  if [ -f ${HOME}/$1 ] || [ -d ${HOME}/$1 ] ; then
-    mv ${HOME}/$1 ${HOME}/$1.orig
+function check_and_remove {
+  # check if it is a symlink
+  if [ -h ${HOME}/$1 ]; then
+    rm ${HOME}/$1
   fi
 
-  ln -s ${PWD}/$1 ${HOME}
+  # check if it is file or directory
+  if [ -f ${HOME}/$1 ] || [ -d ${HOME}/$1 ]; then
+    mv ${HOME}/$1 ${HOME}/$1.orig
+  fi
 }
 
-link .ssh
-link .vim
-link .bash_profile
-link .bash_unsplash
-link .bashrc
-link .gitconfig
-link .gitmessage
-link .tmux.conf
-link .vimrc
-link .git-completion.bash
+files=$(echo .* | fmt -w 1 | sed '/^\.$/d' | sed '/^\.\.$/d' | sed '/^\.git$/d' | sed '/^\.gitignore$/d' )
+files="bin $files"
+
+for file in $files; do
+  if [ -f $file ]; then
+    check_and_remove $file
+    ln -s ${PWD}/$file ${HOME}
+  fi
+
+  if [ -d $file ]; then
+    [ -d ${HOME}/$file ] || mkdir ${HOME}/$file
+    for subfile in $(ls -A $file); do
+      check_and_remove $file/$subfile
+      ln -s ${PWD}/$file/$subfile ${HOME}/$file
+    done
+  fi
+done
+
