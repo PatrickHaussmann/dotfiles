db149154244e2462e1e0a01a6adfa56fb75d28e2
patrickhaussmann <git@patrickhaussmann.de>
2021-05-31 08:20:10 +0200

    Fix jupyter install
    running jupyter notebook needs a non-root user

--- scripts/programs/jupyter.sh
+++ scripts/programs/jupyter.sh
@@ -8,3 +8,6 @@ pip3 install notebook
 
 pip3 install numpy pandas pytest scikit-learn gradio tensorflow keras scipy plotly seaborn matplotlib
 
+# new unprivileged user for running the server
+adduser --disabled-password --gecos "" jupyter
+
