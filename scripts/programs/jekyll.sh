f1d5f003f98f317306bf0b8432abc2690a8da184
patrickhaussmann <git@patrickhaussmann.de>
2021-06-02 22:46:16 +0200

    Use bash dir in jekyll install

--- scripts/programs/jekyll.sh
+++ scripts/programs/jekyll.sh
@@ -6,9 +6,16 @@
 echo "Installing Jekyll"
 sudo apt-get install -y ruby-full build-essential zlib1g-dev
 
-echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
-echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
-echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+
+mkdir -p ~/.bash.d
+
+cat <<EOF > ~/.bash.d/jekyll.bash
+# Install Ruby Gems to ~/gems 
+export GEM_HOME="$HOME/gems"
+export PATH="$HOME/gems/bin:$PATH"
+EOF
+
 source ~/.bashrc
 
 sudo gem install jekyll bundler github-pages
+
