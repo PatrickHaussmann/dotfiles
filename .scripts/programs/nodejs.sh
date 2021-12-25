f2806c237f36279264db71b1c08c42a07a44032d
patrickhaussmann <git@patrickhaussmann.de>
2021-12-25 19:21:25 +0100

    Remove vercel

--- .scripts/programs/nodejs.sh
+++ .scripts/programs/nodejs.sh
@@ -4,8 +4,7 @@
 
 echo "Installing nodejs+npm"
 
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs npm
 
-yes | sudo npm install -g serve vercel prettier
+yes | sudo npm install -g serve prettier
 
