dc6610cab918fd08d410b1ff8d5fa33016f802c8
patrickhaussmann <git@patrickhaussmann.de>
2022-01-16 11:30:48 +0000

    Fix node install

--- Install.Software/nodejs.sh
+++ Install.Software/nodejs.sh
@@ -2,12 +2,9 @@
 
 echo "Installing nodejs+npm"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-nvm install node
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 yes | sudo npm install -g serve prettier live-server
 
