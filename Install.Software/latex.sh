53ea9dbb53f90b741e8b77ad256270e73ef093fc
Patrick Haußmann <git@patrickhaussmann.de>
2022-02-10 18:24:20 +0100

    Update latex.sh

--- Install.Software/latex.sh
+++ Install.Software/latex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Installing LaTex"
-sudo apt install -y texlive texlive-lang-german texlive-latex-extra
+sudo apt install -y texlive texlive-lang-german texlive-latex-extra latexmk texlive-lang-english texlive-plain-generic texlive-science lmodern
 
