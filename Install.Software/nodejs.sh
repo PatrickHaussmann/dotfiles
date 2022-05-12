b86cb30151fa0390c9ccf14253a31946d00df975
patrickhaussmann <git@patrickhaussmann.de>
2022-05-12 21:05:56 +0200

    Fix node install

--- Install.Software/nodejs.sh
+++ Install.Software/nodejs.sh
@@ -3,7 +3,7 @@
 echo "Installing nodejs+npm"
 
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 yes | sudo npm install -g serve prettier live-server
